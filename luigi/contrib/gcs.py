891d00622d095973727e6e27c2f8705249a56e64
Brendan Greenley <brendang@spotify.com>
2021-09-22 17:06:40 -0400

    add gcs decompressive transcoding support

--- luigi/contrib/gcs.py
+++ luigi/contrib/gcs.py
@@ -53,6 +53,14 @@ except ImportError:
 else:
     RETRYABLE_ERRORS = (httplib2.HttpLib2Error, IOError)
 
+try:
+    from google.cloud import storage
+    from google.cloud.exceptions import NotFound
+    import gzip  # used by NewGCSClient, only needed if above succeed
+except ImportError:
+    logger.warning("Loading GCS module without the python packages google-cloud-storage. \
+        This will crash at runtime if NewGCSClient is used.")
+
 # Number of bytes to send/receive in each request.
 CHUNKSIZE = 10 * 1024 * 1024
 
@@ -67,6 +75,38 @@ EVENTUAL_CONSISTENCY_MAX_SLEEPS = 300
 
 # Uri for batch requests
 GCS_BATCH_URI = 'https://storage.googleapis.com/batch/storage/v1'
+
+# Used for decompressive transcoding, when enabled (NewGCSClient only.)
+GZIPPABLE_MIMETYPES = [
+    'application/javascript',
+    'application/json',
+    'application/jsonlines',
+    'application/rss+xml',
+    'application/x-javascript',
+    'application/x-jsonlines',
+    'application/x-ndjson',
+    'application/xhtml+xml',
+    'application/xml',
+    'text/css',
+    'text/csv',
+    'text/html',
+    'text/javascript',
+    'text/plain',
+    'text/tab-separated-values',
+    'text/tsv',
+    'text/xml'
+]
+
+# Supplement gaps in the mimetypes package for common data formats (NewGCSClient only.)
+MIMETYPE_MAP = {
+    'avro': 'application/avro',
+    'ndjson': 'application/x-jsonlines'
+}
+
+
+def update_mimetypes(mapping):
+    for type, ext in mapping.items():
+        mimetypes.add_type(type, ext)
 
 
 # Retry configurations. For more details, see https://tenacity.readthedocs.io/en/latest/
@@ -408,6 +448,283 @@ class GCSClient(luigi.target.FileSystem):
                 if chunk_callback(fp):
                     done = True
 
+        return return_fp
+
+
+class NewGCSClient(luigi.target.FileSystem):
+    """An(other) implementation of a FileSystem over Google Cloud Storage.
+
+       This implementation supports on-the-fly decompressive transcoding, which can
+       reduce at-rest storage of non-compressed data by 90%+. put_multiple() is
+       limited to a single process as the underlying Google client cannot be pickled.
+    """
+
+    def __init__(self, credentials=None, descriptor='', http_=None, chunksize=CHUNKSIZE, gcs_client_info=None,
+                 gcp_project=None, mimetype_map=MIMETYPE_MAP, gzippable_mimetypes=GZIPPABLE_MIMETYPES,
+                 decompressive_transcoding=False, **client_options_kwargs):
+        self.chunksize = chunksize
+        self.gzippable_mimetypes = gzippable_mimetypes
+        self.decompressive_transcoding = decompressive_transcoding
+        self.client = storage.Client(project=gcp_project, credentials=credentials, _http=http_,
+                                     client_info=gcs_client_info, client_options=client_options_kwargs)
+        update_mimetypes(mimetype_map)
+
+    def _path_to_bucket_and_key(self, path):
+        (scheme, netloc, path, _, _) = urlsplit(path)
+        assert scheme == 'gs'
+        path_without_initial_slash = path[1:]
+        return netloc, path_without_initial_slash
+
+    def _is_root(self, key):
+        return len(key) == 0 or key == '/'
+
+    def _add_path_delimiter(self, key):
+        return key if key[-1:] == '/' else key + '/'
+
+    @gcs_retry
+    def _obj_exists(self, bucket, obj):
+        bucket = self.client.get_bucket(bucket)
+        blob = bucket.blob(obj)
+        return blob.exists()
+
+    def _list_iter(self, bucket, prefix, return_blobs=False):
+        blobs = self.client.list_blobs(bucket_or_name=bucket, prefix=prefix)
+        for blob in blobs:
+            if not return_blobs:
+                yield {'name': blob.name, 'updated': blob.updated}
+            else:
+                yield blob
+
+    @gcs_retry
+    def _do_put(self, file_or_filename, dest_path, mimetype, chunksize, content_encoding=None, from_file=False):
+        bucket, obj = self._path_to_bucket_and_key(dest_path)
+        bucket_obj = self.client.get_bucket(bucket)
+        blob = bucket_obj.blob(obj, chunk_size=chunksize)
+        blob.content_encoding = content_encoding
+        blob.content_type = mimetype
+        if from_file:
+            blob.upload_from_file(
+                file_or_filename, size=file_or_filename.tell(), rewind=True)
+        else:
+            blob.upload_from_filename(file_or_filename)
+        _wait_for_consistency(lambda: self._obj_exists(bucket, obj))
+
+    def exists(self, path):
+        bucket, obj = self._path_to_bucket_and_key(path)
+        if self._obj_exists(bucket, obj):
+            return True
+
+        return self.isdir(path)
+
+    def isdir(self, path):
+        bucket, obj = self._path_to_bucket_and_key(path)
+        if self._is_root(obj):
+            try:
+                self.client.get_bucket(bucket)
+            except NotFound:
+                return False
+            except Exception:
+                raise
+
+        obj = self._add_path_delimiter(obj)
+        if self._obj_exists(bucket, obj):
+            return True
+
+        # Any objects with this prefix
+        blobs = self.client.list_blobs(
+            bucket_or_name=bucket, prefix=obj, max_results=20)
+        return bool(list(blobs))
+
+    def remove(self, path, recursive=True):
+        (bucket, obj) = self._path_to_bucket_and_key(path)
+
+        if self._is_root(obj):
+            raise InvalidDeleteException(
+                'Cannot delete root of bucket at path {}'.format(path))
+
+        if self._obj_exists(bucket, obj):
+            self.client.bucket(bucket).delete_blob(obj)
+            _wait_for_consistency(lambda: not self._obj_exists(bucket, obj))
+            return True
+
+        if self.isdir(path):
+            if not recursive:
+                raise InvalidDeleteException(
+                    'Path {} is a directory. Must use recursive delete'.format(path))
+
+            self.client.bucket(bucket).delete_blobs([d['name'] for d in list(
+                self._list_iter(bucket, self._add_path_delimiter(obj)))])
+            _wait_for_consistency(lambda: not self.isdir(path))
+            return True
+
+        return False
+
+    def put(self, filename, dest_path, mimetype=None, chunksize=None, decompressive_transcoding=None):
+        chunksize = chunksize or self.chunksize
+        content_encoding = None
+        mimetype = mimetype or mimetypes.guess_type(dest_path)[0] or DEFAULT_MIMETYPE
+        if decompressive_transcoding is None:
+            decompressive_transcoding = self.decompressive_transcoding
+        if decompressive_transcoding and mimetype in self.gzippable_mimetypes:
+            content_encoding = 'gzip'
+            with tempfile.NamedTemporaryFile(delete=True, mode='wb') as fp:
+                with open(filename, 'rb') as f_in, gzip.open(fp, 'wb') as f_out:
+                    f_out.writelines(f_in)
+                self._do_put(fp.name, dest_path, mimetype, chunksize, content_encoding)
+        else:
+            self._do_put(filename, dest_path, mimetype, chunksize, content_encoding)
+
+    def _forward_args_to_put(self, kwargs):
+        return self.put(**kwargs)
+
+    def put_multiple(self, filepaths, remote_directory, mimetype=None, chunksize=None,
+                     decompressive_transcoding=None, num_process=1):
+        if isinstance(filepaths, str):
+            raise ValueError(
+                'filenames must be a list of strings. If you want to put a single file, '
+                'use the `put(self, filename, ...)` method'
+            )
+
+        put_kwargs_list = [
+            {
+                'filename': filepath,
+                'dest_path': os.path.join(remote_directory, os.path.basename(filepath)),
+                'mimetype': mimetype,
+                'chunksize': chunksize,
+                'decompressive_transcoding': decompressive_transcoding
+            }
+            for filepath in filepaths
+        ]
+        if num_process > 1:
+            logger.warning("NewGCSClient is unable to be pickled. Running with num_process=1.")
+        for put_kwargs in put_kwargs_list:
+            self._forward_args_to_put(put_kwargs)
+
+    def put_string(self, contents, dest_path, mimetype=None, decompressive_transcoding=None):
+        content_encoding = None
+        mimetype = mimetype or mimetypes.guess_type(dest_path)[0] or DEFAULT_MIMETYPE
+        assert isinstance(mimetype, str)
+        if not isinstance(contents, bytes):
+            contents = contents.encode("utf-8")
+        if decompressive_transcoding is None:
+            decompressive_transcoding = self.decompressive_transcoding
+        with BytesIO() as f:
+            if decompressive_transcoding and mimetype in self.gzippable_mimetypes:
+                content_encoding = 'gzip'
+                with gzip.GzipFile(fileobj=f, mode='wb', compresslevel=9) as fgz:
+                    fgz.write(contents)
+            else:
+                f.write(contents)
+            self._do_put(f, dest_path, mimetype, chunksize=None, content_encoding=content_encoding, from_file=True)
+
+    def mkdir(self, path, parents=True, raise_if_exists=False):
+        if self.exists(path):
+            if raise_if_exists:
+                raise luigi.target.FileAlreadyExists()
+            elif not self.isdir(path):
+                raise luigi.target.NotADirectory()
+            else:
+                return
+
+        self.put_string(b"", self._add_path_delimiter(
+            path), mimetype='text/plain')
+
+    def copy(self, source_path, destination_path):
+        src_bucket_str, src_obj = self._path_to_bucket_and_key(source_path)
+        dest_bucket_str, dest_obj = self._path_to_bucket_and_key(
+            destination_path)
+
+        src_bucket = self.client.bucket(src_bucket_str)
+        dest_bucket = self.client.bucket(dest_bucket_str)
+
+        if self.isdir(source_path):
+            src_prefix = self._add_path_delimiter(src_obj)
+            dest_prefix = self._add_path_delimiter(dest_obj)
+
+            source_path = self._add_path_delimiter(source_path)
+            copied_objs = []
+            for obj in self.listdir(source_path):
+                suffix = obj[len(source_path):]
+                src_bucket.copy_blob(
+                    src_bucket.blob(
+                        src_prefix + suffix), dest_bucket, dest_prefix + suffix
+                )
+                copied_objs.append(dest_prefix + suffix)
+
+            _wait_for_consistency(
+                lambda: all(self._obj_exists(dest_bucket, obj)
+                            for obj in copied_objs))
+        else:
+            src_bucket.copy_blob(
+                src_bucket.blob(src_obj), dest_bucket, dest_obj
+            )
+            _wait_for_consistency(
+                lambda: self._obj_exists(dest_bucket, dest_obj))
+
+    def rename(self, *args, **kwargs):
+        """
+        Alias for ``move()``
+        """
+        self.move(*args, **kwargs)
+
+    def move(self, source_path, destination_path):
+        """
+        Rename/move an object from one GCS location to another.
+        """
+        self.copy(source_path, destination_path)
+        self.remove(source_path)
+
+    def listdir(self, path):
+        """
+        Get an iterable with GCS folder contents.
+        Iterable contains paths relative to queried path.
+        """
+        bucket, obj = self._path_to_bucket_and_key(path)
+
+        obj_prefix = self._add_path_delimiter(obj)
+        if self._is_root(obj_prefix):
+            obj_prefix = ''
+
+        obj_prefix_len = len(obj_prefix)
+        for it in self._list_iter(bucket, obj_prefix):
+            yield self._add_path_delimiter(path) + it['name'][obj_prefix_len:]
+
+    def list_wildcard(self, wildcard_path):
+        """Yields full object URIs matching the given wildcard.
+        Currently only the '*' wildcard after the last path delimiter is supported.
+        (If we need "full" wildcard functionality we should bring in gsutil dependency with its
+        https://github.com/GoogleCloudPlatform/gsutil/blob/master/gslib/wildcard_iterator.py...)
+        """
+        path, wildcard_obj = wildcard_path.rsplit('/', 1)
+        assert '*' not in path, "The '*' wildcard character is only supported after the last '/'"
+        wildcard_parts = wildcard_obj.split('*')
+        assert len(wildcard_parts) == 2, "Only one '*' wildcard is supported"
+
+        for it in self.listdir(path):
+            if it.startswith(path + '/' + wildcard_parts[0]) and it.endswith(wildcard_parts[1]) and \
+                    len(it) >= len(path + '/' + wildcard_parts[0]) + len(wildcard_parts[1]):
+                yield it
+
+    @gcs_retry
+    def download(self, path, chunksize=None, chunk_callback=lambda _: False):
+        """Downloads the object contents to local file system.
+        Optionally stops after the first chunk for which chunk_callback returns True.
+        """
+        chunksize = chunksize or self.chunksize
+
+        bucket_str, obj_str = self._path_to_bucket_and_key(path)
+
+        bucket = self.client.bucket(bucket_str)
+        blob = bucket.blob(obj_str)
+        data_to_write = []
+        with tempfile.NamedTemporaryFile(delete=False, mode='ab') as fp:
+            for _ in (True,):
+                with blob.open("rb", chunk_size=chunksize) as f:
+                    data_to_write = f.read(chunksize)
+                    fp.write(data_to_write)
+                    if chunk_callback(fp):
+                        break
+            return_fp = _DeleteOnCloseFile(fp.name, 'rb')
         return return_fp
 
 
--- test/contrib/gcs_test.py
+++ test/contrib/gcs_test.py
@@ -44,6 +44,7 @@ TEST_FOLDER = os.environ.get('TRAVIS_BUILD_ID', 'gcs_test_folder')
 
 CREDENTIALS, _ = google.auth.default()
 ATTEMPTED_BUCKET_CREATE = False
+SKIP_NEW_CLIENT_TESTS = False
 
 
 def bucket_url(suffix):
@@ -70,6 +71,35 @@ class _GCSBaseTestCase(unittest.TestCase):
 
         self.client.remove(bucket_url(''), recursive=True)
         self.client.mkdir(bucket_url(''))
+
+    def tearDown(self):
+        self.client.remove(bucket_url(''), recursive=True)
+
+
+class _GCSBaseTestCaseNewClient(unittest.TestCase):
+    def setUp(self):
+        global SKIP_NEW_CLIENT_TESTS
+        try:
+            from google.cloud import storage
+            from google.api_core.exceptions import Conflict
+        except ImportError:
+            SKIP_NEW_CLIENT_TESTS = True
+            raise unittest.SkipTest('Unable to load google-cloud-storage module')
+        if not SKIP_NEW_CLIENT_TESTS:
+            self.client = gcs.NewGCSClient(CREDENTIALS)
+
+            global ATTEMPTED_BUCKET_CREATE
+            if not ATTEMPTED_BUCKET_CREATE:
+                try:
+                    self.client.client.create_bucket(
+                        project=PROJECT_ID, bucket_or_name=BUCKET_NAME)
+                except Conflict:  # bucket already exists
+                    pass
+
+                ATTEMPTED_BUCKET_CREATE = True
+
+            self.client.remove(bucket_url(''), recursive=True)
+            self.client.mkdir(bucket_url(''))
 
     def tearDown(self):
         self.client.remove(bucket_url(''), recursive=True)
@@ -177,7 +207,176 @@ class GCSClientTest(_GCSBaseTestCase):
 
 
 @pytest.mark.gcloud
+@pytest.mark.skipif(SKIP_NEW_CLIENT_TESTS, reason="google-cloud-storage not installed")
+class NewGCSClientTest(_GCSBaseTestCaseNewClient):
+    def test_not_exists(self):
+        self.assertFalse(self.client.exists(bucket_url('does_not_exist')))
+        self.assertFalse(self.client.isdir(bucket_url('does_not_exist')))
+
+    def test_exists(self):
+        self.client.put_string('hello', bucket_url('exists_test'))
+        self.assertTrue(self.client.exists(bucket_url('exists_test')))
+        self.assertFalse(self.client.isdir(bucket_url('exists_test')))
+
+    def test_mkdir(self):
+        self.client.mkdir(bucket_url('exists_dir_test'))
+        self.assertTrue(self.client.exists(bucket_url('exists_dir_test')))
+        self.assertTrue(self.client.isdir(bucket_url('exists_dir_test')))
+
+    def test_mkdir_by_upload(self):
+        self.client.put_string('hello', bucket_url('test_dir_recursive/yep/file'))
+        self.assertTrue(self.client.exists(bucket_url('test_dir_recursive')))
+        self.assertTrue(self.client.isdir(bucket_url('test_dir_recursive')))
+
+    def test_download(self):
+        self.client.put_string('hello', bucket_url('test_download'))
+        fp = self.client.download(bucket_url('test_download'))
+        self.assertEqual(b'hello', fp.read())
+
+    def test_rename(self):
+        self.client.put_string('hello', bucket_url('test_rename_1'))
+        self.client.rename(bucket_url('test_rename_1'), bucket_url('test_rename_2'))
+        self.assertFalse(self.client.exists(bucket_url('test_rename_1')))
+        self.assertTrue(self.client.exists(bucket_url('test_rename_2')))
+
+    def test_rename_recursive(self):
+        self.client.mkdir(bucket_url('test_rename_recursive'))
+        self.client.put_string('hello', bucket_url('test_rename_recursive/1'))
+        self.client.put_string('hello', bucket_url('test_rename_recursive/2'))
+        self.client.rename(bucket_url('test_rename_recursive'), bucket_url('test_rename_recursive_dest'))
+        self.assertFalse(self.client.exists(bucket_url('test_rename_recursive')))
+        self.assertFalse(self.client.exists(bucket_url('test_rename_recursive/1')))
+        self.assertTrue(self.client.exists(bucket_url('test_rename_recursive_dest')))
+        self.assertTrue(self.client.exists(bucket_url('test_rename_recursive_dest/1')))
+
+    def test_remove(self):
+        self.client.put_string('hello', bucket_url('test_remove'))
+        self.client.remove(bucket_url('test_remove'))
+        self.assertFalse(self.client.exists(bucket_url('test_remove')))
+
+    def test_remove_recursive(self):
+        self.client.mkdir(bucket_url('test_remove_recursive'))
+        self.client.put_string('hello', bucket_url('test_remove_recursive/1'))
+        self.client.put_string('hello', bucket_url('test_remove_recursive/2'))
+        self.client.remove(bucket_url('test_remove_recursive'))
+
+        self.assertFalse(self.client.exists(bucket_url('test_remove_recursive')))
+        self.assertFalse(self.client.exists(bucket_url('test_remove_recursive/1')))
+        self.assertFalse(self.client.exists(bucket_url('test_remove_recursive/2')))
+
+    def test_listdir(self):
+        self.client.put_string('hello', bucket_url('test_listdir/1'))
+        self.client.put_string('hello', bucket_url('test_listdir/2'))
+
+        self.assertEqual([bucket_url('test_listdir/1'), bucket_url('test_listdir/2')],
+                         list(self.client.listdir(bucket_url('test_listdir/'))))
+        self.assertEqual([bucket_url('test_listdir/1'), bucket_url('test_listdir/2')],
+                         list(self.client.listdir(bucket_url('test_listdir'))))
+
+    def test_put_file(self):
+        with tempfile.NamedTemporaryFile() as fp:
+            lorem = b'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt\n'
+            # Larger file than chunk size, fails with incorrect progress set up
+            big = lorem * 41943
+            fp.write(big)
+            fp.flush()
+
+            self.client.put(fp.name, bucket_url('test_put_file'))
+            self.assertTrue(self.client.exists(bucket_url('test_put_file')))
+            self.assertEqual(big, self.client.download(bucket_url('test_put_file')).read())
+
+    def test_put_string(self):
+        lorem = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt\n'
+        big = lorem * 41943
+
+        self.client.put_string(big, bucket_url('test_put_file'))
+        self.assertTrue(self.client.exists(bucket_url('test_put_file')))
+        self.assertEqual(big, self.client.download(bucket_url('test_put_file')).read().decode('utf-8'))
+
+    def test_put_file_transcoding(self):
+        with tempfile.NamedTemporaryFile() as fp:
+            lorem = b'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt\n'
+            # Larger file than chunk size, fails with incorrect progress set up
+            big = lorem * 41943
+            fp.write(big)
+            fp.flush()
+            size_written = os.stat(fp.name).st_size
+
+            # gzippable mimetype, should compress
+            self.client.put(fp.name, bucket_url('test_gzip_file'), mimetype='text/plain', decompressive_transcoding=True)
+            bucket = self.client.client.get_bucket(BUCKET_NAME)
+            blob = bucket.get_blob(TEST_FOLDER + '/test_gzip_file')
+            self.assertNotEqual(blob.size, size_written)
+            self.assertEqual(blob.content_encoding, 'gzip')
+            self.assertTrue(self.client.exists(bucket_url('test_gzip_file')))
+            self.assertEqual(big, self.client.download(bucket_url('test_gzip_file')).read())
+
+            # non-gzippable mimetype, should not compress
+            self.client.put(fp.name, bucket_url('test_non_gzip_file'), mimetype='application/octet-stream', decompressive_transcoding=True)
+            bucket = self.client.client.get_bucket(BUCKET_NAME)
+            blob = bucket.get_blob(TEST_FOLDER + '/test_non_gzip_file')
+            self.assertEqual(blob.size, size_written)
+            self.assertNotEqual(blob.content_encoding, 'gzip')
+            self.assertTrue(self.client.exists(bucket_url('test_non_gzip_file')))
+            self.assertEqual(big, self.client.download(bucket_url('test_non_gzip_file')).read())
+
+            # gzippable mimetype, default to client default of no decompressive-transcoding, should not compress
+            self.client.put(fp.name, bucket_url('test_non_gzip_file_2'), mimetype='text/plain')
+            bucket = self.client.client.get_bucket(BUCKET_NAME)
+            blob = bucket.get_blob(TEST_FOLDER + '/test_non_gzip_file_2')
+            self.assertEqual(blob.size, size_written)
+            self.assertNotEqual(blob.content_encoding, 'gzip')
+            self.assertTrue(self.client.exists(bucket_url('test_non_gzip_file_2')))
+            self.assertEqual(big, self.client.download(bucket_url('test_non_gzip_file_2')).read())
+
+    def test_put_file_multiproc(self):
+        temporary_fps = []
+        for _ in range(2):
+            fp = tempfile.NamedTemporaryFile(mode='wb')
+
+            lorem = b'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt\n'
+            # Larger file than chunk size, fails with incorrect progress set up
+            big = lorem * 41943
+            fp.write(big)
+            fp.flush()
+            temporary_fps.append(fp)
+
+        filepaths = [f.name for f in temporary_fps]
+        self.client.put_multiple(filepaths, bucket_url(''), num_process=2)
+
+        for fp in temporary_fps:
+            basename = os.path.basename(fp.name)
+            self.assertTrue(self.client.exists(bucket_url(basename)))
+            self.assertEqual(big, self.client.download(bucket_url(basename)).read())
+            fp.close()
+
+
+@pytest.mark.gcloud
 class GCSTargetTest(_GCSBaseTestCase, FileSystemTargetTestMixin):
+
+    def create_target(self, format=None):
+        return gcs.GCSTarget(bucket_url(self.id()), format=format, client=self.client)
+
+    def test_close_twice(self):
+        # Ensure gcs._DeleteOnCloseFile().close() can be called multiple times
+        tgt = self.create_target()
+
+        with tgt.open('w') as dst:
+            dst.write('data')
+        assert dst.closed
+        dst.close()
+        assert dst.closed
+
+        with tgt.open() as src:
+            assert src.read().strip() == 'data'
+        assert src.closed
+        src.close()
+        assert src.closed
+
+
+@pytest.mark.gcloud
+@pytest.mark.skipif(SKIP_NEW_CLIENT_TESTS, reason="google-cloud-storage not installed")
+class GCSTargetNewGCSClientTest(_GCSBaseTestCaseNewClient, FileSystemTargetTestMixin):
 
     def create_target(self, format=None):
         return gcs.GCSTarget(bucket_url(self.id()), format=format, client=self.client)
